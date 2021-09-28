d70b459fe736bb2614a8e1b4de05772556f29726
Merlin Do <doduc9569@gmail.com>
2021-09-28 10:06:10 +0700

    add option swap word

--- src/Word.java
+++ src/Word.java
@@ -11,6 +11,11 @@ public class Word {
         this.word_explain = word_explain;
     }
 
+    public Word(Word word) {
+        this.word_target = word.getWord_target();
+        this.word_explain = word.getWord_explain();
+    }
+
     public void setWord_target(String word_target) {
         this.word_target = word_target;
     }
@@ -27,7 +32,12 @@ public class Word {
     public void showWord() {
         System.out.printf("%s %s \n", word_target, word_explain);
     }
-
+    public boolean equals(Word ortherWord) {
+        if (word_target.equals(ortherWord.getWord_target()) && word_explain.equals(ortherWord.getWord_explain())) {
+            return true;
+        }
+        return false;
+    }
 
     public String toString() {
         int lengthTarget = word_target.length();
@@ -38,5 +48,14 @@ public class Word {
         for(int i = 0; i< 25 - lengthExplain; i++)  res += " ";
         res += this.word_explain;
         return res;
+    }
+
+    public void swap (Word ortherWord) {
+        Word temp = new Word(ortherWord);
+        ortherWord.setWord_explain(word_explain);
+        ortherWord.setWord_target(word_target);
+
+        word_target = temp.getWord_target();
+        word_explain = word_explain;
     }
 }
