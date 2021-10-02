9883e677ad3c8dec9a106fb99ecadca91d0cd8b3
Merlin Do <doduc9569@gmail.com>
2021-10-02 21:00:09 +0700

    basic command line

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,8 +1,75 @@
+import java.util.Locale;
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 public class DictionaryManagement {
-    public static void insertFromCommandLine(){
-        Scanner sc = new Scanner(System.in);
-        Word.word_target = sc.nextLine();
+    protected Dictionary dictionary;
+    DictionaryManagement() {
+        dictionary = new Dictionary();
+    }
+    Scanner sc = new Scanner(System.in);
+
+    /**
+     * insert word to dictionary
+     */
+    public void insertFromCommandLine()
+    {
+        System.out.print("Target word: ");
+        String word_target = sc.nextLine();
+        word_target = word_target.toLowerCase(Locale.ROOT);
+        System.out.print("Explain word: ");
+        String word_explain = sc.nextLine();
+        word_explain = word_explain.toLowerCase(Locale.ROOT);
+        Word word = new Word(word_target, word_explain);
+        dictionary.addWordToDictionary(word);
+    }
+    public void insertFromCommandLine(Word word) {
+        dictionary.addWordToDictionary(word);
+    }
+
+    /**
+     * remove from command line
+     */
+    public void removeFromCommandLine() {
+        System.out.print("Target word: ");
+        String word_target = sc.nextLine();
+        word_target = word_target.toLowerCase(Locale.ROOT);
+        System.out.print("Explain word: ");
+        String word_explain = sc.nextLine();
+        word_explain = word_explain.toLowerCase(Locale.ROOT);
+        Word word = new Word(word_target, word_explain);
+        if (dictionary.containsWordInDictionary(word)) {
+            dictionary.removeWordInDictionary(word);
+            System.out.println("This word is remove dictionary");
+        }
+        else {
+            System.out.println("This word is't contain");
+        }
+    }
+
+    public void removeFromCommandLine(Word word) {
+        dictionary.removeWordInDictionary(word);
+    }
+
+    /**
+     * print all word in dictionary
+     */
+    public void showAllWord() {
+        for (int i = 0; i < dictionary.getLengthDictionary(); i++) {
+            System.out.printf("%-5d ", i);
+            dictionary.getInfoWord(i);
+        }
+    }
+
+    public void sortDictionary() {
+        dictionary.sortDictionary();
+    }
+
+    public int indexOf(Word word) {
+        return dictionary.indexOf(word);
+    }
+    public void insertFromFile() {
+        File file = new File ("dictionaries.txt");
     }
 }
