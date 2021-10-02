542fca407bf570bb0d9a50fa7ee1061b640d2e57
Merlin Do <doduc9569@gmail.com>
2021-10-03 00:01:18 +0700

    data

---
+++ src/DictionaryCommandLine.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class DictionaryCommandLine extends DictionaryManagement{
+    Scanner sc = new Scanner(System.in);
+
+    DictionaryCommandLine() {
+        super();
+    }
+
+    public void dictionaryBasic() {
+        insertFromCommandLine();
+        showAllWord();
+    }
+    public void showAllWord() {
+        for (int i = 0; i < dictionary.getLengthDictionary(); i++) {
+            System.out.printf("%-5d ", i);
+            dictionary.getInfoWord(i);
+        }
+    }
+
+
+
+}
