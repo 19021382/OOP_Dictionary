704afeea5ff9e8ce44e12e573ca37db856eeb891
Merlin Do <doduc9569@gmail.com>
2021-10-20 12:46:35 +0700

    add GUI

---
+++ src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -0,0 +1,293 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class DictionaryApplication extends Application{
+    Stage window;
+    Scene scene1, scene2, controlWord;
+    private DictionaryManagement dictionary;
+    DictionaryApplication() {
+        window = new Stage();
+        dictionary = new DictionaryManagement();
+        dictionary.insertFromFile();
+        dictionary.writeToFile();
+        scene2 = searchScene();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+
+        //scene 1
+        Label label = new Label("Main Application Dictionary");
+        label.setFont(new Font("Arial", 20));
+        label.setTextFill(Color.RED);
+        label.setFont(Font.font(null, FontWeight.BOLD, 50));
+        label.setTranslateY(100);
+        label.setTranslateX(50);
+        label.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            int count = 0;
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (count % 2 == 0) {
+                    label.setScaleY(1.2);
+                    label.setScaleX(1.2);
+                } else {
+                    label.setScaleY(1);
+                    label.setScaleX(1);
+                }
+                count ++;
+            }
+        });
+
+        Button exit = new Button("EXIT");
+        exit.setLayoutX(400);
+        exit.setLayoutY(100);
+        exit.setLineSpacing(50);
+        exit.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        Button addWord = new Button("Goto add word");
+        addWord.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+
+        addWord.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == addWord) {
+                addWordGUI();
+                //window.setScene(scene2);
+            }
+        });
+
+        exit.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == exit) {
+                window.close();
+            }
+        });
+
+        HBox layoutAddWord = new HBox();
+        layoutAddWord.getChildren().addAll(addWord, new Label("                      "), exit);
+        layoutAddWord.setSpacing(75);
+        layoutAddWord.setAlignment(Pos.BASELINE_CENTER);
+        layoutAddWord.getBoundsInParent();
+        //layoutAddWord.setAlignment(Pos.BOTTOM_RIGHT);
+        //layoutAddWord.getChildren().add(new Label("      "));
+
+        //search
+        Button search = new Button("Search");
+        search.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        search.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == search) {
+                //addWordGUI();
+                window.setScene(scene2);
+            }
+        });
+
+
+        Button showAll = new Button("SHOW ALL WORD");
+        showAll.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        showAll.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == showAll) {
+
+            }
+        });
+        // layout search
+        HBox layoutSearch = new HBox();
+        layoutSearch.getChildren().addAll(search, showAll);
+        layoutSearch.setAlignment(Pos.BASELINE_CENTER);
+        layoutSearch.setSpacing(95);
+
+        VBox layout1 = new VBox();
+        layout1.setSpacing(100);
+        layout1.getChildren().addAll(label, layoutAddWord, layoutSearch);
+        scene1 = new Scene(layout1,800, 600, Color.RED);
+
+        // scene Search
+        Button goBack = new Button("BACK");
+        goBack.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == goBack) {
+                window.setScene(scene1);
+                //addWordGUI();
+            }
+        });
+
+        window.setScene(scene1);
+        window.setTitle("DICTIONARY");
+
+        window.show();
+    }
+
+
+    public void addWordGUI() {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("ADD WORD TO DICTIONARY");
+        dialog.setHeaderText("Fill in the gaps");
+        ButtonType loginButtonType = new ButtonType("ADD WORD", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField userName = new TextField();
+        TextField passWord = new TextField();
+        passWord.setPromptText("Word explain ");
+        grid.add(new Label("Word target :"), 0 , 0);
+        grid.add(userName, 1, 0);
+        grid.add(new Label("Word explain :"), 0, 1);
+        grid.add(passWord, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+
+        dialog.setResultConverter(dialogButton ->{
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(userName.getText(), passWord.getText());
+            }
+            return null;
+        });
+        Optional<Pair<String, String>> res = dialog.showAndWait();
+        res.ifPresent(userNamePassword -> {
+            String wordTarget = userNamePassword.getKey();
+            String wordExplain = userNamePassword.getValue();
+            if (!wordTarget.trim().isEmpty() && !wordExplain.trim().isEmpty()) {
+                System.out.println(wordTarget);
+                System.out.println(wordExplain);
+                Word word = new Word(wordTarget, wordExplain);
+                dictionary.insertFromCommandLine(word);
+                dictionary.sortDictionary();
+                dictionary.writeToFile();
+            }
+        });
+    }
+
+    private Scene searchScene() {
+        Label sameTitile = new Label ("                     WORD SEARCH");
+        sameTitile.setFont(new Font("Arial", 20));
+        //sameTitile.setBackground(new BackgroundFill(Color.RED));
+        sameTitile.setAlignment(Pos.BASELINE_CENTER);
+
+        TextField word = new TextField();
+        word.setPromptText("searching");
+
+        HBox layoutSearch1 = new HBox();
+        Button search = new Button("SEARCH");
+        search.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        Button back = new Button("BACK");
+        back.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        Label label = new Label();
+
+        layoutSearch1.getChildren().addAll(search, new Label("                        "), back);
+        layoutSearch1.setAlignment(Pos.BASELINE_RIGHT);
+        layoutSearch1.setSpacing(175);
+
+        back.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == back) {
+                window.setScene(scene1);
+            }
+        });
+
+
+        VBox layoutWord = new VBox();
+        search.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == search) {
+                Label labelButton = new Label();
+                dictionary.showAllWord();
+                String s = word.getText();
+                if (!s.trim().isEmpty()) {
+                    List<Button> list = new LinkedList<Button>();
+                    List<Word> words = dictionary.lookup(s);
+                    for (int i = 0; i < words.size(); i++) {
+                        Button newBt = new Button(words.get(i).toString());
+                        newBt.setFont(new Font("Arial", 20));
+                        int finalI = i;
+                        newBt.setOnAction(actionEvent1 -> {
+                            if (actionEvent1.getSource() == newBt) {
+                                window.setScene(changeWord(words.get(finalI)));
+                            }
+                        });
+                        layoutWord.getChildren().add(newBt);
+                    }
+                    System.out.println();
+                }
+            }
+        });
+
+        HBox line1 = new HBox();
+        Label inLine1 = new Label("         English               Vietnamese");
+        inLine1.setFont(new Font("Arial", 20));
+        line1.getChildren().addAll(inLine1);
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(sameTitile, word, layoutSearch1, line1, label, layoutWord);
+        layout.setSpacing(30);
+
+        /*StackPane layout1 = new StackPane();
+        layout1.getChildren().addAll(back);
+
+         */
+
+        Scene scene = new Scene(layout, 800, 600);
+        return scene;
+    }
+
+    private Scene changeWord(Word word) {
+        Label word_target = new Label ("Word Target         " + word.getWordTarget());
+
+        HBox h1 = new HBox();
+        word_target.setFont(Font.font(null, FontWeight.BOLD, 20));
+        h1.getChildren().addAll(word_target);
+
+        Label word_explain = new Label ("Word explain        " + word.getWordExplain());
+        word_explain.setFont(Font.font(null, FontWeight.BOLD, 20));
+        HBox h2 = new HBox();
+        h2.getChildren().addAll(word_explain);
+
+        Button back = new Button("BACK");
+        back.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        back.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == back) {
+                window.setScene(scene1);
+            }
+        });
+
+        HBox removeHBox = new HBox();
+        Button remove = new Button("REMOVE");
+        remove.setStyle("-fx-font: 25 arial; -fx-base: #b6e7c9;");
+        removeHBox.getChildren().addAll(remove);
+        remove.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == remove) {
+                int index = dictionary.indexOf(word);
+                dictionary.removeFromCommandLine(word);
+                System.out.println("da xoa di tu" + word.toString());
+                window.setScene(scene1);
+            }
+        });
+
+        TextField setTarget = new TextField();
+        HBox target = new HBox();
+        Button setter = new Button("SET WORD EXPLAIN");
+        target.getChildren().addAll(setter, setTarget);
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(h1, h2, removeHBox, target, back);
+        layout.setSpacing(50);
+        Scene scene = new Scene(layout, 500, 700, Color.RED);
+
+        return scene;
+    }
+}
+
+
--- src/main/java/com/example/dictionary/HelloApplication.java
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -1,23 +1,154 @@
-package com.example.dictionary;
+package com.example.javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.*;
-import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 import javafx.util.Pair;
 
 import java.io.IOException;
 import java.util.Optional;
 
-public class HelloApplication extends Application {
-    private boolean run = true;
+import static javafx.scene.paint.Color.*;
+
+public class HelloApplication extends Application{
+    Button button;
+    final String pathFile = "dictionaries.txt";
+    protected DictionaryManagement dictionary = new DictionaryManagement();
+
+    public static void main(String[] args) {
+        launch();
+
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+
+        Button button = new Button("dme m hoc di");
+
+        StackPane layout = new StackPane();
+        Scene scene1 = new Scene(layout, 500, 700);
+        stage.setScene(scene1);
+        stage.show();
+
+        stage.setTitle("Dictionary");
+        button = new Button("search");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("title");
+                alert.setHeaderText("Alert heading");
+                alert.setContentText("choose");
+
+                ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+                ButtonType no = new ButtonType("No", ButtonBar.ButtonData.NO);
+                ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+                alert.getButtonTypes().setAll(yes, no, cancel);
+                Optional<ButtonType> res = alert.showAndWait();
+
+                if (res.get().getButtonData() == ButtonBar.ButtonData.YES) {
+                    System.out.println("Yes");
+                }
+                if (res.get().getButtonData() == ButtonBar.ButtonData.NO) {
+                    System.out.println("No");
+                }
+                if (res.get().getButtonData() == ButtonBar.ButtonData.CANCEL_CLOSE) {
+                    System.out.println("Cancel");
+                }
+
+                String message = res.get().getText();
+                Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+                alert1.setTitle("alert1");
+                alert1.setHeaderText("Heading 1");
+                alert1.setContentText("content alert1");
+
+            }
+        });
+        //Button button1 = new Button("remove");
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(button);
+        Scene scene = new Scene(layout, 500, 700);
+        stage.setScene(scene);
+        stage.show();
+
+        Label label = new Label("Các chức năng của Từ điển");
+        CheckBox box1 = new CheckBox("add word");
+        CheckBox box2 = new CheckBox("remove word");
+        CheckBox box3 = new CheckBox("change information word");
+
+        Button button = new Button();
+        button.setText("hello ban oi");
+        button.setMaxHeight(100);
+        button.setMaxWidth(150);
+        button.setOnAction(actionEvent -> {
+            if (actionEvent.getSource() == button) {
+                addWordGUI();
+            }
+        });
+
+        box1.setSelected(true);
+        Button button1 = new Button("submit");
+        button1.setLayoutX(300);
+        button1.setLayoutY(300);
+        button1.setOnAction(e -> {
+            String choose = null;
+            if (box1.isSelected()) {
+                choose = box1.getText();
+            }
+            if (box2.isSelected()) {
+                choose = box2.getText();
+            }
+            if (box3.isSelected()) {
+                choose = box3.getText();
+            }
+            System.out.println(choose);
+
+        });
+
+        HBox layoutH = new HBox(20);
+        layoutH.getChildren().addAll(box1, box2, box3);
+
+        VBox layoutV = new VBox(50);
+        layoutV.getChildren().addAll(label, layoutH, button1, button);
+        Scene scene = new Scene(layoutV, 500, 700);
+        stage.setScene(scene);
+        stage.show();
+
+         */
+
+        DictionaryApplication app = new DictionaryApplication();
+        try {
+            app.start(stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
-    public void addWordGUI() {
+    }
+
+    public static void runApplication(Stage stage) {
+
+    }
+
+    public static void addWordGUI() {
         Dialog<Pair<String, String>> dialog = new Dialog<>();
         dialog.setTitle("ADD WORD TO DICTIONARY");
         dialog.setHeaderText("Fill in the gaps");
@@ -30,7 +161,7 @@ public class HelloApplication extends Application {
         grid.setPadding(new Insets(20, 150, 10, 10));
 
         TextField userName = new TextField();
-        userName.setPromptText("Word target");
+        //userName.setPromptText("Word target");
 
         //PasswordField passWord = new PasswordField();
 
@@ -59,66 +190,10 @@ public class HelloApplication extends Application {
             String wordExplain = userNamePassword.getValue();
             System.out.println(wordExplain);
             System.out.println(wordTarget);
+            Word word = new Word(wordTarget, wordExplain);
+
         });
     }
 
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-
-         */
-        /*Dialog<Pair<String, String>> dialog = new Dialog<>();
-        dialog.setTitle("this is title");
-        dialog.setHeaderText("Add to dictionary");
-
-        ButtonType addWord = new ButtonType("Add Word", ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(addWord, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
-        TextField wordTarget = new TextField();
-        wordTarget.setPromptText("Word Target");
-        PasswordField word = new PasswordField();
-        word.setPromptText("Word Explain");
-
-        grid.add(new Label("Word Target"), 0, 0);
-        grid.add(wordTarget, 1, 0);
-        grid.add(new Label("Word Explain"), 0, 1);
-        grid.add(word,1, 1);
-
-        Node add = dialog.getDialogPane().lookupButton(addWord);
-        add.setDisable(true);
-
-        wordTarget.textProperty().addListener((observable, oldValue, newValue) -> {
-            word.textProperty().addListener((observable1, oldValue1, newValue1) -> {
-                boolean check = newValue.trim().isEmpty() && newValue1.trim().isEmpty();
-                add.setDisable(check);
-            });
-        });
-
-        dialog.getDialogPane().setContent(add);
-        dialog.resultConverterProperty(dialogButton -> {
-            if (dialogButton == ButtonData.OK_DONE) {
-
-            }
-        });
-
-         */
-
-        while (run) {
-            addWordGUI();
-        }
-}
-
-    public static void main(String[] args) {
-        launch();
-    }
 }
